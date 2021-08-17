0881e7eb4487af1aab25c702ab8435d87fdcc740
Sandsten <staffansandberg@outlook.com>
2021-08-17 14:42:36 +0200

    Readme updates

--- README.md
+++ README.md
@@ -1,3 +1,68 @@
 # Base app template with Webpack  
 
 Use this as a starting point for web apps
+
+
+
+# Webpack
+Bundler which keep track of all files your project relies upon. It will then bundle these together into a small number of bundles(files) which can be loaded by the browser. It keeps track of which files depend on each other and builds a "dependency graph".
+## Config file
+Relies upon 6 core concepts described below.
+### Entry
+File which should be used as the starting point when building the dependency tree. normally `./src/index.js`. But in this case `./src/index.ts` since we are using typescript.
+
+```javascript
+module.exports = {
+  entry: './path/to/my/entry/file.js',
+};
+```
+
+### Output
+Where to spit out the bundles it creates and how to name them.
+```javascript
+const path = require('path');
+
+module.exports = {
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'my-first-webpack.bundle.js',
+  },
+};
+```
+### Loaders
+Out of the box Webpack only understands `.js` and `.json` files. Loaders allow Webpack to use other filetypes. It can then process them and make them available to you as modules. `import * as blablabla from "this is a module";`
+
+`rules.test` is to select which files and `rules.use` is which loader to use for processing the file.
+```js
+module.exports = {
+  module: {
+    rules: [{ 
+      test: /\.txt$/, 
+      use: 'raw-loader' 
+    }],
+  },
+};
+```
+
+### Plugins
+Can perform a wider range of tasks compared to loaders. 
+```js
+const HtmlWebpackPlugin = require('html-webpack-plugin'); //installed via npm
+const webpack = require('webpack'); //to access built-in plugins
+
+module.exports = {
+  plugins: [new HtmlWebpackPlugin({ template: './src/index.html' })],
+};
+```
+In this example `html-webpack-plugin` generates a base html file for your project automatically.
+
+### Mode
+Can be set to `production`, `development` or `none`. Activates webpack's built in optimization tailored to each use case.
+```js
+module.exports = {
+  mode: 'production',
+};
+```
+
+### Browser compatibility
+Supports all browsers which are ES5-compliant.
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,67 +1,68 @@
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-	target: 'web',
-	mode: 'development',
-	output: {
-		publicPath: '/',
-	},
-	devServer: {
-		host: '0.0.0.0', // Required for working inside a docker container
-		port: 3001,
+  target: 'web',
+  mode: 'development',
+  entry: './src/index.ts',
+  output: {
+    publicPath: '/',
+  },
+  devServer: {
+    host: '0.0.0.0', // Required for working inside a docker container
+    port: 3001,
     overlay: true,
-		historyApiFallback: true, // 404 responses will fall back to index.html. Required for using react-router-dom
-		// disableHostCheck: true // Might have to be enabled in order to run dev server in docker container
-	},
-	devtool: 'source-map', // Remove this when in production, takes alot of space!
-	resolve: {
-		// This is the order in which extensions are tested against imports
-		// e.g we can use "import File from '../path/to/file';" instead of "import File from '../path/to/file.ts';""
-		extensions: ['.ts', '.tsx', '.js', '.jsx'],
-	},
-	module: {
-		rules: [
-			{
-				test: /\.(t|j)sx?$/,
-				use: { loader: 'ts-loader' },
-				exclude: /node_modules/,
-			},
-			{
-				test: /\.glsl$/,
-				loader: 'webpack-glsl-loader',
-			},
-			{
-				test: /\.scss$/,
-				// Order of modules matters
-				use: [
-					process.env.NODE_ENV !== 'production' ? 'style-loader' : MiniCssExtractPlugin.loader,
-					'css-loader',
-					'sass-loader',
-				],
-			},
-			{
-				test: /\.(jpg|png|svg|gif|pdf)$/,
-				use: ['file-loader'],
-			},
-			{
-				//Load fonts
-				test: /\.(woff|woff2|eot|ttf|otf)$/,
-				use: ['file-loader'],
-			},
-			{
-				test: /\.html$/,
-				use: [
-					{
-						loader: 'html-loader',
-						options: { minimize: true },
-					},
-				],
-			},
-		],
-	},
-	plugins: [
-		new HtmlWebPackPlugin({
-			template: './src/index.html',
-		})
-	],
+    historyApiFallback: true, // 404 responses will fall back to index.html. Required for using react-router-dom
+    // disableHostCheck: true // Might have to be enabled in order to run dev server in docker container
+  },
+  devtool: 'source-map', // Remove this when in production, takes alot of space!
+  resolve: {
+    // This is the order in which extensions are tested against imports
+    // e.g we can use "import File from '../path/to/file';" instead of "import File from '../path/to/file.ts';""
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(t|j)sx?$/,
+        use: { loader: 'ts-loader' },
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.glsl$/,
+        loader: 'webpack-glsl-loader',
+      },
+      {
+        test: /\.scss$/,
+        // Order of modules matters
+        use: [
+          process.env.NODE_ENV !== 'production' ? 'style-loader' : MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.(jpg|png|svg|gif|pdf|webp)$/,
+        use: ['file-loader'],
+      },
+      {
+        //Load fonts
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: ['file-loader'],
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+            options: { minimize: true },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/index.html',
+    }),
+  ],
 };
